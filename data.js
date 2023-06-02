09e1b60c07f1b780d8f67be424d256e3d41e4e6d
AnasAxa <anasaxa45@gmail.com>
2023-06-03 00:07:26 +0400

    Upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cashierview",
+      "name": "CashierView",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4981259646154381,
+          "pitch": 0.13372618753685117,
+          "rotation": 0,
+          "target": "1-centerview"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-centerview",
+      "name": "CenterView",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0640004507992522,
+          "pitch": 0.2893340739547057,
+          "rotation": 0,
+          "target": "0-cashierview"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
